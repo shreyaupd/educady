15d36fea736262965beca312c7820e67f71c4708
shreyaupd <shreyaupd234@gmail.com>
2025-01-18 12:37:08 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from "./Components/Navbar/Navbar"
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <Title subTitle='Our Programs' title='What We Offer'/>
       <Programs/>
     </div>
   )
--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -3,17 +3,32 @@ import './Programs.css'
 import propic1 from '../../assets/propic1.jpg'
 import propic2 from '../../assets/propic2.jpg'
 import propic3 from '../../assets/propic3.jpg'
+import program_icon_1 from '../../assets/program-icon-1.png'
+import program_icon_2 from '../../assets/program-icon-2.png'
+import program_icon_3 from '../../assets/program-icon-3.png'
 const Programs = () => {
   return (
-    <div className='programs '>
+    <div className='programs'>
         <div className="program">
             <img src={propic1} alt="" />
+            <div className="caption">
+          <img src={program_icon_1} alt="" />
+          <p>Graduation Degree</p>
+        </div>
         </div>
         <div className="program">
             <img src={propic2} alt="" />
+            <div className="caption">
+          <img src={program_icon_2} alt="" />
+          <p>Undergraduate Degree</p>
+        </div>
         </div>
         <div className="program">
             <img src={propic3} alt="" />
+            <div className="caption">
+          <img src={program_icon_3} alt="" />
+          <p>Postgraduate Degree</p>
+        </div>
         </div>
     </div>
   )
